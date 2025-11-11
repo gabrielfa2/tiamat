afb1633c1329ab26c550e284b8e55f359e761d6b
Gabriel <eugabrielfa07@gmail.com>
2025-11-11 20:20:05 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // Adicione esta linha:
+  base: '/tiamat/', 
+
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
