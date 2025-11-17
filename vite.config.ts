bc184078064c7ff2e4a75134e12ccc13579ccbb7
Gabriel <eugabrielfa07@gmail.com>
2025-11-17 15:58:07 -0300

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,17 @@
+// vite.config.ts
+
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  // Adicione esta linha:
-  base: '/tiamat/', 
+export default defineConfig(({ command }) => ({
+  // AQUI ESTÁ A MUDANÇA:
+  // Se o comando for 'build' (para o deploy), use '/tiamat/'
+  // Caso contrário (no 'serve' do Bolt), use a raiz '/'
+  base: command === 'build' ? '/tiamat/' : '/',
 
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
-});
+}));
