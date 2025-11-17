a1e2a7ba23812a2b3e5a09ae0e6020946be8ee55
Gabriel <eugabrielfa07@gmail.com>
2025-11-17 16:51:44 -0300

    Updated Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -55,7 +55,7 @@ const Header = () => {
 
       {/* Main Navigation */}
       <header className={`
-        bg-gray-900 border-b border-black /* <-- MUDANÇA: Cor do Header */
+        bg-gray-900 border-b border-gray-600 /* <-- MUDANÇA: Cor do Header */
         transition-all duration-300 ease-in-out
         ${isScrolled
           ? 'fixed top-0 left-0 right-0 z-40'
